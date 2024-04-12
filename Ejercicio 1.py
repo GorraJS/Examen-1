9e8eb96d9c881e7e4e9b10ac666967519512f5b8
GorraJS <gorrahabla1234@gmail.com>
2024-04-12 10:21:40 -0300

    v2

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,1 +1,9 @@
-asda
+arr = []
+
+def intersect(list1,list2):
+    for i in range(0,len(list1),1):
+        for j in range(0,len(list1),1):
+            if list1[i] == list2[j]:
+                arr.append(list1[i])
+    return arr
+print(intersect([1,2,3,4,5],[3,4,5,6,7]))
